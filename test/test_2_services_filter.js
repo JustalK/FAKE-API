bbf19900cf0692d0f3035500bcd8dca6aef0c321
justalk <justal.kevin@gmail.com>
2021-12-09 08:00:01 +0100

    [FIX] Fixing the project

--- test/test_2_services_filter.js
+++ test/test_2_services_filter.js
@@ -4,7 +4,6 @@ require('module-alias/register')
 const test = require('ava')
 const m = require('@src/services/utils/filter')
 const constants = require('@src/libs/constants')
-const model = require('@src/models/user')
 
 test('[STATIC] handle_limit_argument with null argument', t => {
   const rsl = m.handle_limit_argument(null)
@@ -23,20 +22,6 @@ test('[STATIC] handle_order_argument', t => {
 
 test('[STATIC] handle_order_argument with bad argument', t => {
   t.throws(() => { m.handle_order_argument('test') })
-})
-
-test('[STATIC] handle_sort_argument', t => {
-  const rsl = m.handle_sort_argument('username', model)
-  t.is(rsl, 'username')
-})
-
-test('[STATIC] handle_sort_argument with bad argument', t => {
-  t.throws(() => { m.handle_sort_argument('test', model) })
-})
-
-test('[STATIC] handle_joint_argument', t => {
-  const rsl = m.handle_joint_argument(constants.joint_and, model)
-  t.is(rsl, constants.joint_and)
 })
 
 test('[STATIC] handle_joint_argument with bad argument', t => {
