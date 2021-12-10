272586bbf1ea473a7ea54404bd28f76d627e3096
justalk <justal.kevin@gmail.com>
2021-12-10 07:47:28 +0100

    [FEATURE] Creating the schema for post

---
+++ src/models/post.js
@@ -0,0 +1,36 @@
+/**
+* The models of posts
+* @module models/post
+*/
+'use strict'
+
+const mongoose = require('mongoose')
+const path = require('path')
+const filename = path.basename(__filename, '.js')
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  deleted: {
+    type: Boolean
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+  collection: filename,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  admin_bro: {
+    listProperties: ['title', 'sentance']
+  }
+})
+
+module.exports = mongoose.model(filename, schema)
---
+++ src/services/utils/post.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { gql } = require('apollo-server-express')
+
+/**
+ * The post object
+ * @typedef {Object} Post
+ * @property {string} name The title of the post
+ * @property {string} content The title of the post
+ * @property {boolean} deleted True if the post is deleted, or else false
+ */
+
+module.exports = gql`
+  """
+  The models for the post
+  """
+  type Post {
+    """
+    The id of a post
+    """
+    _id: String!
+    """
+    The title of a post
+    """
+    title: String!
+    """
+    The content of a post
+    """
+    content: String!
+    """
+    True if the post is deleted or not
+    """
+    deleted: Boolean
+  }
+`
