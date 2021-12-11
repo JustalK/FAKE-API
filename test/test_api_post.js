aef2bf4f30a448ffa1afc2377e2e79ffb827bbfb
justalk <justal.kevin@gmail.com>
2021-12-11 14:51:24 +0100

    [TEST] Adding test for get

--- test/test_api_post.js
+++ test/test_api_post.js
@@ -3,6 +3,7 @@
 require('dotenv').config({ path: './env/.env.test' })
 require('isomorphic-fetch')
 require('module-alias/register')
+const queries_post = require('@test/queries/post')
 
 const test = require('ava')
 const m = require('@src')
@@ -14,5 +15,8 @@ test.before(async () => {
 })
 
 test('[VISITOR] Get a post by id', async t => {
-  t.is(true, true)
+  const response = await queries_post.get_post_by_id({ post_id: '5fd5b58efbc2f7a33c2aa001' })
+  t.is(response.get_post_by_id.title, 'The only post not randomly created')
+  t.is(response.get_post_by_id.content, 'The content of the only post not created randomly.')
+  t.is(response.get_post_by_id.deleted, false)
 })
