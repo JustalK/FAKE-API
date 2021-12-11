afe3089b6c382fbbd38be229e0f932dd7ca1f498
justalk <justal.kevin@gmail.com>
2021-12-11 14:28:31 +0100

    [REFACTORING] Adding the test folder for seeding
    [DOCUMENTATION] Adding documentation

--- seeding/seeder.js
+++ seeding/seeder.js
@@ -19,7 +19,7 @@ module.exports = {
   /**
   * Seed the database with the informations in data
   **/
-  seed: async (folder = 'datas') => {
+  seed: async (folder = 'test') => {
     const seeder = module.exports.get_seeder({
       database: 'mongodb://' + process.env.DB_USER_DATA + ':' + process.env.DB_PASS_DATA + '@' + process.env.DB_URI_URL + ':' + process.env.DB_URI_PORT + '/' + process.env.DB_NAME + process.env.DB_URI_AUTHENTICATION_DB,
       dropDatabase: true
---
+++ seeding/test/post/posts.js
@@ -0,0 +1,27 @@
+'use strict'
+const faker = require('faker')
+
+const mongoose = require('mongoose')
+
+/**
+* Create n random posts in the database
+* @param {number} number_post The number of post to create
+* @return {[Post]} Return the random post created
+**/
+const creating_fake_posts = (number_post) => {
+  return Array.from({ length: number_post }, () => ({
+    title: faker.lorem.words(),
+    content: faker.lorem.paragraphs(),
+    deleted: faker.datatype.boolean()
+  }))
+}
+
+module.exports = [
+  ...creating_fake_posts(process.env.SEEDING_NUMBER),
+  {
+    id: mongoose.Types.ObjectId('5fd5b58efbc2f7a33c2aa001'),
+    title: 'The only post not randomly created',
+    content: 'The content of the only post not created randomly.',
+    deleted: false
+  }
+]
