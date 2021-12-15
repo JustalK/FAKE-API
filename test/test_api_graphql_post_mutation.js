f3d6f3b8cab1f704b7499884067349d864b62e3e
justalk <justal.kevin@gmail.com>
2021-12-15 22:10:04 +0100

    [TEST] Changing name for graphql

--- test/test_api_graphql_post_mutation.js
+++ test/test_api_graphql_post_mutation.js
@@ -14,35 +14,35 @@ test.before(async () => {
   await m_seeding.seed()
 })
 
-test('[VISITOR] Add a post by args', async t => {
+test('[VISITOR][GRAPHQL] Add a post by args', async t => {
   const response = await queries_post.add_post_by_args({ title: 'My test title', content: 'My test content' })
   t.is(response.add_post_by_args.title, 'My test title')
   t.is(response.add_post_by_args.content, 'My test content')
 })
 
-test('[VISITOR] Add a post by args with missing content arg', async t => {
+test('[VISITOR][GRAPHQL] Add a post by args with missing content arg', async t => {
   const response = await queries_post.add_post_by_args({ title: 'My test title' })
   t.is(response.errors[0].message, 'Field "add_post_by_args" argument "content" of type "String!" is required, but it was not provided.')
 })
 
-test('[VISITOR] Add a post by args with missing title arg', async t => {
+test('[VISITOR][GRAPHQL] Add a post by args with missing title arg', async t => {
   const response = await queries_post.add_post_by_args({ content: 'My test content' })
   t.is(response.errors[0].message, 'Field "add_post_by_args" argument "title" of type "String!" is required, but it was not provided.')
 })
 
-test('[VISITOR] Add a post by args with wrong title type', async t => {
+test('[VISITOR][GRAPHQL] Add a post by args with wrong title type', async t => {
   const response = await queries_post.error_add_post_by_args({ title: 12, content: 'My test content' })
   t.is(response.errors[0].message, 'String cannot represent a non string value: 12')
 })
 
-test('[VISITOR] Update a post', async t => {
+test('[VISITOR][GRAPHQL] Update a post', async t => {
   const response = await queries_post.update_post_by_id('5fd5b58efbc2f7a33c2aa001', { title: 'Updated title', content: 'The updated content', deleted: false })
   t.is(response.update_post_by_id.title, 'Updated title')
   t.is(response.update_post_by_id.content, 'The updated content')
   t.is(response.update_post_by_id.deleted, false)
 })
 
-test('[VISITOR] Delete a post', async t => {
+test('[VISITOR][GRAPHQL] Delete a post', async t => {
   const response = await queries_post.delete_post_by_id('5fd5b58efbc2f7a33c2aa001')
   t.is(response.delete_post_by_id, true)
   const response_deleted = await queries_post.delete_post_by_id('5fd5b58efbc2f7a33c2aa001')
--- test/test_api_graphql_post_query.js
+++ test/test_api_graphql_post_query.js
@@ -15,42 +15,42 @@ test.before(async () => {
   await m_seeding.seed()
 })
 
-test('[VISITOR] Get a post by id', async t => {
+test('[VISITOR][GRAPHQL] Get a post by id', async t => {
   const response = await queries_post.get_post_by_id({ post_id: '5fd5b58efbc2f7a33c2aa001' })
   t.is(response.get_post_by_id.title, 'The only post not randomly created')
   t.is(response.get_post_by_id.content, 'The content of the only post not created randomly.')
   t.is(response.get_post_by_id.deleted, false)
 })
 
-test('[VISITOR] Trying to get a post by id which does not exist', async t => {
+test('[VISITOR][GRAPHQL] Trying to get a post by id which does not exist', async t => {
   const response = await queries_post.get_post_by_id({ post_id: '5fd5b58efbc2f7a33c2aa000' })
   t.is(response.errors[0].message, 'The object with id(5fd5b58efbc2f7a33c2aa000) does not exist.')
   t.is(response.errors[0].extensions.code, 'NOT_FOUND')
 })
 
-test('[VISITOR] Trying to get a post by id with an invalid mongoose ID', async t => {
+test('[VISITOR][GRAPHQL] Trying to get a post by id with an invalid mongoose ID', async t => {
   const response = await queries_post.get_post_by_id({ post_id: '5fd5b58efbc2f7' })
   t.is(response.errors[0].message, 'Expected value of type "ID!", found "5fd5b58efbc2f7"; The id(5fd5b58efbc2f7) is not a valid ID.')
   t.is(response.errors[0].extensions.code, 'BAD_ID')
 })
 
-test('[VISITOR] Get all posts without filters', async t => {
+test('[VISITOR][GRAPHQL] Get all posts without filters', async t => {
   const response = await queries_post.get_posts({})
   // The random post + the static one
   t.is(response.get_posts.length, parseInt(process.env.SEEDING_NUMBER) + 1)
 })
 
-test('[VISITOR] Get all posts with limit to 1', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with limit to 1', async t => {
   const response = await queries_post.get_posts({ limit: 1 })
   t.is(response.get_posts.length, 1)
 })
 
-test('[VISITOR] Get all posts with limit to 12', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with limit to 12', async t => {
   const response = await queries_post.get_posts({ limit: 12 })
   t.is(response.get_posts.length, 12)
 })
 
-test('[VISITOR] Get all posts with skip', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with skip', async t => {
   const response_full = await queries_post.get_posts({})
   const second_response = response_full.get_posts[1]
   const response_skipped = await queries_post.get_posts({ skip: 1 })
@@ -58,7 +58,7 @@ test('[VISITOR] Get all posts with skip', async t => {
   t.is(response_skipped.get_posts[0].content, second_response.content)
 })
 
-test('[VISITOR] Get all posts with skip and limit', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with skip and limit', async t => {
   const response_full = await queries_post.get_posts({ limit: 2 })
   const second_response = response_full.get_posts[1]
   t.is(response_full.get_posts.length, 2)
@@ -68,7 +68,7 @@ test('[VISITOR] Get all posts with skip and limit', async t => {
   t.is(response_skipped.get_posts.length, 2)
 })
 
-test('[VISITOR] Get all posts with order', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with order', async t => {
   const response_ascending = await queries_post.get_posts({ order: constants.order_ascending })
   const last_response_ascending = response_ascending.get_posts[response_ascending.get_posts.length - 1]
   const response_descending = await queries_post.get_posts({ order: constants.order_descending })
@@ -76,49 +76,49 @@ test('[VISITOR] Get all posts with order', async t => {
   t.is(response_descending.get_posts[0].content, last_response_ascending.content)
 })
 
-test('[VISITOR] Get all posts with filter on title', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with filter on title', async t => {
   const response = await queries_post.get_posts({ title: /The only post/ })
   t.is(response.get_posts.length, 1)
   t.is(response.get_posts[0].title, 'The only post not randomly created')
   t.is(response.get_posts[0].content, 'The content of the only post not created randomly.')
 })
 
-test('[VISITOR] Get bo posts with filter on title', async t => {
+test('[VISITOR][GRAPHQL] Get bo posts with filter on title', async t => {
   const response = await queries_post.get_posts({ title: /zzZZZzzzzZZZ/ })
   t.is(response.get_posts.length, 0)
 })
 
-test('[VISITOR] Get all posts with filter on content', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with filter on content', async t => {
   const response = await queries_post.get_posts({ content: /content of/ })
   t.is(response.get_posts.length, 1)
   t.is(response.get_posts[0].title, 'The only post not randomly created')
   t.is(response.get_posts[0].content, 'The content of the only post not created randomly.')
 })
 
-test('[VISITOR] Get no posts with filter on content', async t => {
+test('[VISITOR][GRAPHQL] Get no posts with filter on content', async t => {
   const response = await queries_post.get_posts({ content: /Idontexist/ })
   t.is(response.get_posts.length, 0)
 })
 
-test('[VISITOR] Get no posts with filter on title and content with joint and', async t => {
+test('[VISITOR][GRAPHQL] Get no posts with filter on title and content with joint and', async t => {
   const response = await queries_post.get_posts({ title: /The only post/, content: /zzZZZzzzzZZZ/, joint: constants.joint_and })
   t.is(response.get_posts.length, 0)
 })
 
-test('[VISITOR] Get all posts with filter on title and content with joint or', async t => {
+test('[VISITOR][GRAPHQL] Get all posts with filter on title and content with joint or', async t => {
   const response = await queries_post.get_posts({ title: /The only post/, content: /zzZZZzzzzZZZ/, joint: constants.joint_or })
   t.is(response.get_posts.length, 1)
   t.is(response.get_posts[0].title, 'The only post not randomly created')
   t.is(response.get_posts[0].content, 'The content of the only post not created randomly.')
 })
 
-test('[VISITOR] Get all posts sorted with title and content', async t => {
+test('[VISITOR][GRAPHQL] Get all posts sorted with title and content', async t => {
   const response_sorted_title = await queries_post.get_posts({ sort: 'title' })
   const response_sorted_content = await queries_post.get_posts({ sort: 'content' })
   t.not(response_sorted_title.get_posts[0], response_sorted_content.get_posts[0])
 })
 
-test('[VISITOR] Get all posts sorted with a key that does not exist', async t => {
+test('[VISITOR][GRAPHQL] Get all posts sorted with a key that does not exist', async t => {
   const response = await queries_post.get_posts({ sort: 'try' })
   t.is(response.errors[0].message, 'The key (try) is not available for sorting')
 })
